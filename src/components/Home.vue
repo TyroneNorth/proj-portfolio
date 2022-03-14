684e48dbc24bd599f55aca54c96e95d57e019c39
Tyrone North <tnorthjr@gmail.com>
2022-03-14 12:24:29 -0500

    Added dependencies and components
    -Added Home Component

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+
+</script>
+<button class="std-btn">Hello</button>
+<button class="btn btn-primary"></button>
+
+<template>
+
+
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import bootstrap from 'bootstrap'
+import './assets/css/global.css'
 
 createApp(App).mount('#app')
